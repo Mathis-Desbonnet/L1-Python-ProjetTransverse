c9064e257ddbb8d8434a72945b01c2da30a0eea2
Mathis-Desbonnet <mathis.desbonnet@gmail.com>
2024-02-08 17:16:46 +0100

    First Commit

--- main.py
+++ main.py
@@ -2,26 +2,27 @@ import pygame
 from player import Player
 from plateform import Platform
 
-class Main():
+
+class Main:
     def __init__(self) -> None:
         self.screen = pygame.display.set_mode((1920, 1080))
         self.running = True
-        self.player = Player()
-        self.platform = Platform()
-        
+        self.imageBack = pygame.image.load("./assets/sky_background.png")
+
     def draw(self):
-        self.screen.fill((255, 255, 255))
-        pygame.draw.rect(self.screen, (0, 0, 0), (100, 100, 100, 100))
-        
+        self.screen.blit(self.imageBack, (0, 0))
+
     def refreshScreen(self):
         self.draw()
         pygame.display.flip()
-    
+
     def run(self):
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-                        
+
             self.refreshScreen()
+
+
 Main().run()
