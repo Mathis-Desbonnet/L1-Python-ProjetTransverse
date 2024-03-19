79ca90b2466bef74ebaf577aabaf22b96c18939d
LouisMarieFort <louis-marie.fort@efrei.net>
2024-03-19 12:56:34 +0100

    modification defineSpeedWithAngle

--- fonctionTrajectoireY.py
+++ fonctionTrajectoireY.py
@@ -1,3 +1,5 @@
+from math import sin, cos, sqrt, pi
+
 def ySerieBasicJump(g: float, y: int, initialSpeed: int) -> float:
     """
     Returns the height 'u(n+1)' of the player
@@ -11,7 +13,8 @@ def defineSpeedWithAngle(angle: int, platformSpeed: int, initialYSpeedForBasicJu
     """
     Returns the x speed and the y speed for a bumper jump
     """
-    return int(platformSpeed * (80 - angle) / 90), int(initialYSpeedForBasicJump * angle / 90)
+    angle = angle * pi / 180
+    return int(cos(angle) * platformSpeed), int(sin(angle) * platformSpeed)
 
 """
 g = 9.8
@@ -25,3 +28,8 @@ while y < nextPlatformHeight:
     currentSpeed = ySerieBasicJump(g, y, currentSpeed)[1]
     print(y)
 """
+
+print(defineSpeedWithAngle(80, 40, 10))
+print(defineSpeedWithAngle(0, 40, 10))
+print(defineSpeedWithAngle(45, 40, 10))
+print(45*pi/180)
