236d6d6dbe74b42f7d9b4602eb9c3f51225063a1
Arnaud <chadal.arnaud2@gmail.com>
2024-03-19 17:15:57 +0100

    Add quit button

--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
 import pygame
-
+from main import Main
 
 class Main:
     def __init__(self) -> None:
@@ -10,44 +10,55 @@ class Main:
         self.imageLaunch2 = pygame.image.load("./assets/buttonLaunch2.png").convert_alpha()
         self.imageStory = pygame.image.load("./assets/buttonStory.png").convert_alpha()
         self.imageStory2 = pygame.image.load("./assets/buttonStory2.png").convert_alpha()
+        self.imageQuit = pygame.image.load("./assets/buttonQuit.png").convert_alpha()
+        self.imageQuit2 = pygame.image.load("./assets/buttonQuit2.png").convert_alpha()
 
         self.clock = pygame.time.Clock()
         self.tick = 0
         self.buttonNbr = 0
         self.upIn = False
         self.downIn = False
+        self.spaceIn = False
 
     def isKeySpacePressed(self):
         keys = pygame.key.get_pressed() 
         if keys[pygame.K_DOWN] :
-            if not self.downIn :
-                self.buttonNbr = 1
+            if not self.downIn and self.buttonNbr < 2:
+                self.buttonNbr += 1
                 self.downIn = True
                 print(self.buttonNbr)
         else : self.downIn = False
 
         if keys[pygame.K_UP]:
-            if not self.upIn :
-                self.buttonNbr = 0
+            if not self.upIn and self.buttonNbr > 0:
+                self.buttonNbr -= 1
                 self.upIn = True
                 print(self.buttonNbr)
         else : self.upIn = False
 
         if keys[pygame.K_SPACE] :
             if not self.spaceIn :
-                if buttonNbr == 0 :
+                if self.buttonNbr == 0 : #Launch
                     print("Et là on passe au jeu :D")
-                elif buttonNbr == 1 :
-                    print("Il entre un poulet et ressort une tourte")
+                elif self.buttonNbr == 1 : #Story
+                    print("Il y entre un poulet et ressort une tourte")
+                else : self.running = False #Quit game
 
     def draw(self):
         self.screen.blit(self.imageBack, (0, 0))
         if self.buttonNbr == 0 :
             self.screen.blit(self.imageLaunch2, (704, 100))
             self.screen.blit(self.imageStory, (704, 300))
-        else :
+            self.screen.blit(self.imageQuit, (704, 500))
+        elif self.buttonNbr == 1 :
             self.screen.blit(self.imageLaunch, (704, 100))
             self.screen.blit(self.imageStory2, (704, 300))
+            self.screen.blit(self.imageQuit, (704, 500))
+        else :
+            self.screen.blit(self.imageLaunch, (704, 100))
+            self.screen.blit(self.imageStory, (704, 300))
+            self.screen.blit(self.imageQuit2, (704, 500))
+
 
     def refreshScreen(self):
         self.draw()
