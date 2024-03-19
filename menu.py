1a17d6e2d20adae2fd57631a964c14f658e920a2
Arnaud <chadal.arnaud2@gmail.com>
2024-03-19 13:29:46 +0100

    Add sprites

--- menu.py
+++ menu.py
@@ -6,6 +6,11 @@ class Main:
         self.screen = pygame.display.set_mode((1920, 1080))
         self.running = True
         self.imageBack = pygame.image.load("./assets/sky_background.png").convert_alpha()
+        self.imageLaunch = pygame.image.load("./assets/buttonLaunch.png").convert_alpha()
+        self.imageLaunch2 = pygame.image.load("./assets/buttonLaunch2.png").convert_alpha()
+        self.imageStory = pygame.image.load("./assets/buttonStory.png").convert_alpha()
+        self.imageStory2 = pygame.image.load("./assets/buttonStory2.png").convert_alpha()
+
         self.clock = pygame.time.Clock()
         self.tick = 0
         self.buttonNbr = 0
@@ -16,14 +21,14 @@ class Main:
         keys = pygame.key.get_pressed() 
         if keys[pygame.K_DOWN] :
             if not self.downIn :
-                self.buttonNbr += 1
+                self.buttonNbr = 1
                 self.downIn = True
                 print(self.buttonNbr)
         else : self.downIn = False
 
         if keys[pygame.K_UP]:
             if not self.upIn :
-                self.buttonNbr -= 1
+                self.buttonNbr = 0
                 self.upIn = True
                 print(self.buttonNbr)
         else : self.upIn = False
@@ -37,6 +42,12 @@ class Main:
 
     def draw(self):
         self.screen.blit(self.imageBack, (0, 0))
+        if self.buttonNbr == 0 :
+            self.screen.blit(self.imageLaunch2, (704, 100))
+            self.screen.blit(self.imageStory, (704, 300))
+        else :
+            self.screen.blit(self.imageLaunch, (704, 100))
+            self.screen.blit(self.imageStory2, (704, 300))
 
     def refreshScreen(self):
         self.draw()
