0ce768301a1d061080d487e05b11dfdb18e6c7e1
Arnaud-Chadal <arnaud.chadal@efrei.net>
2024-03-30 18:18:58 +0100

    Menu's background

--- menu.py
+++ menu.py
@@ -4,7 +4,6 @@ class mainMenu:
     def __init__(self) -> None:
         self.screen = pygame.display.set_mode((1920, 1080))
         self.running = True
-        self.imageBack = pygame.image.load("./assets/sky_background.png").convert_alpha()
         self.imageLaunch = pygame.image.load("./assets/button_play_0.png").convert_alpha()
         self.imageLaunch2 = pygame.image.load("./assets/button_play_1.png").convert_alpha()
         self.imageStory = pygame.image.load("./assets/button_story_0.png").convert_alpha()
@@ -12,12 +11,26 @@ class mainMenu:
         self.imageQuit = pygame.image.load("./assets/button_quit_0.png").convert_alpha()
         self.imageQuit2 = pygame.image.load("./assets/button_quit_1.png").convert_alpha()
 
+        self.imageBack = pygame.image.load("./assets/sky_background.png").convert_alpha()
+        self.imagefarground1 = pygame.image.load("./assets/farground_spr1.png").convert_alpha()
+        self.imagefarground2 = pygame.image.load("./assets/farground_spr2.png").convert_alpha()
+        self.imagefrontground1 = pygame.image.load("./assets/frontground_spr1.png").convert_alpha()
+        self.imagefrontground2 = pygame.image.load("./assets/frontground_spr2.png").convert_alpha()
+
         self.clock = pygame.time.Clock()
         self.tick = 0
         self.buttonNbr = 0
         self.upIn = False
         self.downIn = False
         self.spaceIn = False
+
+        self.fargroundSpeed = 1
+        self.frontgroundSpeed = 2
+        self.fargroundX1 = 0
+        self.fargroundX2 = 3072
+        self.frontgroundX1 = 0
+        self.frontgroundX2 = 3072
+
 
     def isKeySpacePressed(self):
         keys = pygame.key.get_pressed() 
@@ -46,18 +59,36 @@ class mainMenu:
 
     def draw(self):
         self.screen.blit(self.imageBack, (0, 0))
+        self.screen.blit(self.imagefarground1, (self.fargroundX1, 0))
+        self.screen.blit(self.imagefarground2, (self.fargroundX2, 0))
+        self.screen.blit(self.imagefrontground1, (self.frontgroundX1, 0))
+        self.screen.blit(self.imagefrontground2, (self.frontgroundX2, 0))
+
+
+        if self.fargroundX1 <= -3072 : self.fargroundX1 = 3072
+        if self.fargroundX2 <= -3072 : self.fargroundX2 = 3072
+
+        self.fargroundX1 = (self.fargroundX1-self.fargroundSpeed)
+        self.fargroundX2 = (self.fargroundX2-self.fargroundSpeed)
+
+        if self.frontgroundX1 <= -3072 : self.frontgroundX1 = 3072
+        if self.frontgroundX2 <= -3072 : self.frontgroundX2 = 3072
+
+        self.frontgroundX1 = (self.frontgroundX1-self.frontgroundSpeed)
+        self.frontgroundX2 = (self.frontgroundX2-self.frontgroundSpeed)
+
         if self.buttonNbr == 0 :
-            self.screen.blit(self.imageLaunch2, (600, 300))
-            self.screen.blit(self.imageStory, (600, 450))
-            self.screen.blit(self.imageQuit, (600, 600))
+            self.screen.blit(self.imageLaunch2, (600, 306))
+            self.screen.blit(self.imageStory, (600, 456))
+            self.screen.blit(self.imageQuit, (600, 606))
         elif self.buttonNbr == 1 :
-            self.screen.blit(self.imageLaunch, (600, 300))
-            self.screen.blit(self.imageStory2, (600, 450))
-            self.screen.blit(self.imageQuit, (600, 600))
+            self.screen.blit(self.imageLaunch, (600, 306))
+            self.screen.blit(self.imageStory2, (600, 456))
+            self.screen.blit(self.imageQuit, (600, 606))
         else :
-            self.screen.blit(self.imageLaunch, (600, 300))
-            self.screen.blit(self.imageStory, (600, 450))
-            self.screen.blit(self.imageQuit2, (600, 600))
+            self.screen.blit(self.imageLaunch, (600, 306))
+            self.screen.blit(self.imageStory, (600, 456))
+            self.screen.blit(self.imageQuit2, (600, 606))
 
 
     def refreshScreen(self):
